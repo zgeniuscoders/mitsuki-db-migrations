2723bafb21f724c882f6a3a7729e949fa3f2fe63
Zgeniuscoders <zgeniuscoders@gmail.com>
2026-03-10 09:49:34 +0100

    chore(config): make Phinx configuration dynamic based on environment
    - Implemented conditional database adapters for MySQL and SQLite.
    - Added dynamic path resolution for migrations and seeds (production vs. testing).
    - Integrated Dotenv for environment-specific database credentials.
    - Configured custom migration templates and environment switching logic.

--- phinx.php
+++ phinx.php
@@ -5,10 +5,32 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+$adapter = $_ENV['DB_CONNECTION'] == "sqlite" ? [
+    'adapter' => 'sqlite',
+    'name'    => '%%PHINX_CONFIG_DIR%%/database/db',
+    'charset' => 'utf8',
+] : [
+    'adapter' => $_ENV['DB_CONNECTION'] ?? 'mysql',
+    'host'    => $_ENV['DB_HOST'] ?? '127.0.0.1',
+    'name'    => $_ENV['DB_DATABASE'] ?? 'mitsuki_db',
+    'user'    => $_ENV['DB_USERNAME'] ?? 'root',
+    'pass'    => $_ENV['DB_PASSWORD'] ?? '',
+    'port'    => $_ENV['DB_PORT'] ?? '3306',
+    'charset' => 'utf8',
+];
+
+$migrationsPath = $_ENV['ENVIRONEMENT'] == "production" ?
+    '%%PHINX_CONFIG_DIR%%/database/migrations'
+    : '%%PHINX_CONFIG_DIR%%/tests/database/migrations';
+
+$seedsPath = $_ENV['ENVIRONEMENT'] == "production" ?
+    '%%PHINX_CONFIG_DIR%%/database/seeds'
+    : '%%PHINX_CONFIG_DIR%%/tests/database/seeds';
+
 return [
     'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/tests/database/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/database/seeds'
+        'migrations' => $migrationsPath,
+        'seeds' => $seedsPath
     ],
     'templates' => [
         'file' => '%%PHINX_CONFIG_DIR%%/stubs/migration_template.txt'
@@ -16,15 +38,7 @@ return [
     'environments' => [
         'default_migration_table' => 'phinxlog',
         'default_environment' => $_ENV['ENVIRONEMENT'] == "production" ? "development" : "testing",
-        'development' => [
-            'adapter' => $_ENV['DB_CONNECTION'] ?? 'mysql',
-            'host'    => $_ENV['DB_HOST'] ?? '127.0.0.1',
-            'name'    => $_ENV['DB_DATABASE'] ?? 'mitsuki_db',
-            'user'    => $_ENV['DB_USERNAME'] ?? 'root',
-            'pass'    => $_ENV['DB_PASSWORD'] ?? '',
-            'port'    => $_ENV['DB_PORT'] ?? '3306',
-            'charset' => 'utf8',
-        ],
+        'development' => $adapter,
         'testing' => [
             'adapter' => 'sqlite',
             'name'    => './tests/database/mitsuki_test',
